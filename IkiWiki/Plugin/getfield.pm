c500bd29f26edb60f4674acc738cadac53e68a2f
Kathryn Andersen <perlkat@katspace.org>
2010-01-24 20:37:34 +1100

    Added ability to access fields from other pages.

--- IkiWiki/Plugin/getfield.pm
+++ IkiWiki/Plugin/getfield.pm
@@ -9,11 +9,11 @@ IkiWiki::Plugin::getfield - query the values of fields
 
 =head1 VERSION
 
-This describes version B<0.01> of IkiWiki::Plugin::getfield
+This describes version B<0.02> of IkiWiki::Plugin::getfield
 
 =cut
 
-our $VERSION = '0.01';
+our $VERSION = '0.02';
 
 =head1 SYNOPSIS
 
@@ -34,7 +34,7 @@ This plugin is meant to be used in conjunction with the B<field> plugin.
 
 One can get the value of a field by using special markup in the page.
 This does not use directive markup, in order to make it easier to
-use the markup inside other directives.  There are two forms:
+use the markup inside other directives.  There are four forms:
 
 =over
 
@@ -50,6 +50,26 @@ For example:
 When the page is processed, this will give you:
 
     <h1>My Long and Complicated Title With Potential For Spelling Mistakes</h1>
+
+=item {{$I<pagename>#I<fieldname>}}
+
+This queries the value of I<fieldname> for the page I<pagename>.
+
+For example:
+
+On PageFoo:
+
+    [[!meta title="I Am Page Foo"]]
+
+    Stuff about Foo.
+
+On PageBar:
+
+    For more info, see [[{{$PageFoo#title}}|PageFoo]].
+
+When PageBar is displayed:
+
+    <p>For more info, see <a href="PageFoo">I Am Page Foo</a>.</p>
 
 =item {{+$I<fieldname>+}}
 
@@ -80,18 +100,44 @@ When PageB is displayed:
     <h1>I Am Page B</h1>
     <p>Stuff about A.</p>
 
+=item {{+$I<pagename>#I<fieldname>+}}
+
+This queries the value of I<fieldname> for the page I<pagename>; the
+only difference between this and {{$I<pagename>#I<fieldname>}} is
+that the full name of I<pagename> is calculated relative to the
+destination page rather than the source page.
+
+I can't really think of a reason why this should be needed, but
+this format has been added for completeness.
+
 =back
+
+=head2 No Value Found
+
+If no value is found for the given field, then the field name is returned.
+
+For example:
+
+On PageFoo:
+
+    [[!meta title="Foo"]]
+    My title is {{$title}}.
+    
+    My description is {{$description}}.
+
+When PageFoo is displayed:
+
+    <p>My title is Foo.</p>
+    
+    <p>My description is description.</p>
+
+This is because "description" hasn't been defined for that page.
 
 =head2 More Examples
 
 Listing all the sub-pages of the current page:
 
     [[!map pages="{{$page}}/*"]]
-
-=head2 LIMITATIONS
-
-One cannot query the values of fields on pages other than the current
-page or the destination page.
 
 =head1 PREREQUISITES
 
@@ -142,16 +188,27 @@ sub do_filter (@) {
     my $page_type=pagetype($page_file);
     if (defined $page_type)
     {
-	# substitute {{$var}} variables (source-page)
-	$params{content} =~ s/{{\$([-\w]+)}}/get_field_value($1,$page)/eg;
+	while ($params{content} =~ /{{\$([-\w\/]+#)?[-\w]+}}/)
+	{
+	    # substitute {{$var}} variables (source-page)
+	    $params{content} =~ s/{{\$([-\w]+)}}/get_field_value($1,$page)/eg;
+
+	    # substitute {{$page#var}} variables (source-page)
+	    $params{content} =~ s/{{\$([-\w\/]+)#([-\w]+)}}/get_other_page_field_value($2,$page,$1)/eg;
+	}
     }
 
     $page_file=$pagesources{$destpage};
     $page_type=pagetype($page_file);
     if (defined $page_type)
     {
-	# substitute {{+$var+}} variables (dest-page)
-	$params{content} =~ s/{{\+\$([-\w]+)\+}}/get_field_value($1,$destpage)/eg;
+	while ($params{content} =~ /{{\+\$([-\w\/]+#)?[-\w]+\+}}/)
+	{
+	    # substitute {{+$var+}} variables (dest-page)
+	    $params{content} =~ s/{{\+\$([-\w]+)\+}}/get_field_value($1,$destpage)/eg;
+	    # substitute {{+$page#var+}} variables (source-page)
+	    $params{content} =~ s/{{\+\$([-\w\/]+)#([-\w]+)\+}}/get_other_page_field_value($2,$destpage,$1)/eg;
+	}
     }
 
     return $params{content};
@@ -160,6 +217,21 @@ sub do_filter (@) {
 #---------------------------------------------------------------
 # Private functions
 # --------------------------------
+sub get_other_page_field_value ($$$) {
+    my $field = shift;
+    my $page = shift;
+    my $other_page = shift;
+
+    my $use_page = bestlink($page, $other_page);
+    my $val = get_field_value($field, $use_page);
+    if ($val eq $field)
+    {
+	return "${other_page}#$field";
+    }
+    return $val;
+
+} # get_other_page_field_value
+
 sub get_field_value ($$) {
     my $field = shift;
     my $page = shift;
@@ -167,8 +239,8 @@ sub get_field_value ($$) {
     my $value = IkiWiki::Plugin::field::field_get_value($field,$page);
     return $value if defined $value;
 
-    # if there is no value, return the unchanged field name.
-    return "{\$$field}";
+    # if there is no value, return the field name.
+    return $field;
 } # get_field_value
 
 1;
