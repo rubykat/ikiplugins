e593fd44a04e08c19c0cfddaa26647aab32d6db2
Kathryn Andersen <perlkat@katspace.org>
2010-07-26 12:31:10 +1000

    Added a ymlfront directive to the ymlfront plugin.
    This directive takes one argument: "data" which contains
    the YAML-format data.

--- IkiWiki/Plugin/ymlfront.pm
+++ IkiWiki/Plugin/ymlfront.pm
@@ -10,11 +10,11 @@ IkiWiki::Plugin::ymlfront - add YAML-format data to a page
 
 =head1 VERSION
 
-This describes version B<0.02> of IkiWiki::Plugin::ymlfront
+This describes version B<0.03> of IkiWiki::Plugin::ymlfront
 
 =cut
 
-our $VERSION = '0.02';
+our $VERSION = '0.03';
 
 =head1 PREREQUISITES
 
@@ -41,6 +41,7 @@ sub import {
 	hook(type => "getsetup", id => "ymlfront", call => \&getsetup);
 	hook(type => "checkconfig", id => "ymlfront", call => \&checkconfig);
 	hook(type => "filter", id => "ymlfront", call => \&filter, first=>1);
+	hook(type => "preprocess", id => "ymlfront", call => \&preprocess, scan=>1);
 	hook(type => "scan", id => "ymlfront", call => \&scan);
 	hook(type => "checkcontent", id => "ymlfront", call => \&checkcontent);
 
@@ -118,6 +119,61 @@ sub scan (@) {
 	$pagestate{$page}{meta}{author} = $pagestate{$page}{ymlfront}{author};
     }
 } # scan
+
+# use this for data in a [[!ymlfront ...]] directive
+sub preprocess (@) {
+    my %params=@_;
+    my $page = $params{page};
+
+    if (! exists $params{data}
+	or ! defined $params{data}
+	or !$params{data})
+    {
+	error gettext("missing data parameter")
+    }
+    # All the work of this is done in scan mode;
+    # when in preprocessing mode, just return an empty string.
+    my $scan=! defined wantarray;
+
+    if (!$scan)
+    {
+	return '';
+    }
+
+    # clear the old data
+    if (exists $pagestate{$page}{ymlfront})
+    {
+	delete $pagestate{$page}{ymlfront};
+    }
+    my $parsed_yml = parse_yml(%params);
+    if (defined $parsed_yml
+	and defined $parsed_yml->{yml})
+    {
+	# save the data to pagestate
+	foreach my $fn (keys %{$parsed_yml->{yml}})
+	{
+	    my $fval = $parsed_yml->{yml}->{$fn};
+	    $pagestate{$page}{ymlfront}{$fn} = $fval;
+	}
+    }
+    # update meta hash
+    if (exists $pagestate{$page}{ymlfront}{title}
+	and $pagestate{$page}{ymlfront}{title})
+    {
+	$pagestate{$page}{meta}{title} = $pagestate{$page}{ymlfront}{title};
+    }
+    if (exists $pagestate{$page}{ymlfront}{description}
+	and $pagestate{$page}{ymlfront}{description})
+    {
+	$pagestate{$page}{meta}{description} = $pagestate{$page}{ymlfront}{description};
+    }
+    if (exists $pagestate{$page}{ymlfront}{author}
+	and $pagestate{$page}{ymlfront}{author})
+    {
+	$pagestate{$page}{meta}{author} = $pagestate{$page}{ymlfront}{author};
+    }
+    return '';
+} # preprocess
 
 sub filter (@) {
     my %params=@_;
@@ -227,17 +283,94 @@ sub parse_yml {
     my $start_of_content = '';
     my $yml_str = '';
     my $rest_of_content = '';
-    if ($content =~ /^---[\n\r](.*?[\n\r])---[\n\r](.*)$/s)
+    if ($params{data})
     {
-	$yml_str = $1;
-	$rest_of_content = $2;
-    } 
-#    elsif ($content =~ /^(.*?[\n\r])---[\n\r](.*?[\n\r])---[\n\r](.*)$/s)
-#    {
-#	$start_of_content = $1;
-#	$yml_str = $2;
-#	$rest_of_content = $3;
-#    } 
+	$yml_str = $params{data};
+    }
+    elsif ($content)
+    {
+	my $regex = qr{
+	    (\\?)		# 1: escape?
+		\[\[(!)		# directive open; 2: prefix
+		(ymlfront)	# 3: command
+		(		# 4: the parameters..
+				\s+	# Must have space if parameters present
+				(?:
+				 (?:[-\w]+=)?		# named parameter key?
+				 (?:
+				  """.*?"""	# triple-quoted value
+				  |
+				  "[^"]*?"	# single-quoted value
+				  |
+				  [^"\s\]]+	# unquoted value
+				 )
+				 \s*			# whitespace or end
+				 # of directive
+				)
+				*)?		# 0 or more parameters
+		\]\]		# directive closed
+	}sx;
+	if ($content =~ /^---[\n\r](.*?[\n\r])---[\n\r](.*)$/s)
+	{
+	    $yml_str = $1;
+	    $rest_of_content = $2;
+	} 
+	elsif ($content =~ /$regex/)
+	{
+	    my $escape=$1;
+	    my $prefix=$2;
+	    my $command=$3;
+	    my $params=$4;
+	    if ($escape)
+	    {
+		$rest_of_content = $content;
+	    }
+	    else
+	    {
+		my %phash = ();
+		while ($params =~ m{
+		       (?:([-\w]+)=)?		# 1: named parameter key?
+		       (?:
+			"""(.*?)"""	# 2: triple-quoted value
+			|
+			"([^"]*?)"	# 3: single-quoted value
+			|
+			(\S+)		# 4: unquoted value
+		       )
+		       (?:\s+|$)		# delimiter to next param
+		       }sgx) {
+		    my $key=$1;
+		    my $val;
+		    if (defined $2) {
+			$val=$2;
+			$val=~s/\r\n/\n/mg;
+			$val=~s/^\n+//g;
+			$val=~s/\n+$//g;
+		    }
+		    elsif (defined $3) {
+			$val=$3;
+		    }
+		    elsif (defined $4) {
+			$val=$4;
+		    }
+
+		    if (defined $key) {
+			$phash{$key} = $val;
+		    }
+		    else {
+			$phash{''} = $val;
+		    }
+		}
+		if (defined $phash{data})
+		{
+		    $yml_str = $phash{data};
+		    $content =~ /^(.*?)\[\[!ymlfront.*?\]\](.*?)$/s;
+		    $start_of_content = $1;
+		    $rest_of_content = $2;
+		}
+	    }
+	}
+    }
     if ($yml_str)
     {
 	# if {{$page}} is there, do an immediate substitution
